524e0e681b7119ca81b072ef1280f2e061d0dff9
dingming <lol>
2023-02-26 15:47:15 +0800

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="home">
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
